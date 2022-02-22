46f77e394fd7b2b5f8bfa620b55f40e7e4ad0156
Bluzzi <camilledugas.pro@gmail.com>
2022-02-22 02:00:05 +0100

    Create Input component

---
+++ src/lib/components/form/Input.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+
+    // Parameters :
+    export let title: string;
+
+    export let type = "text";
+    export let placeholder = "Enter your " + type + "..";
+
+    export let value: string = "";
+
+    // Event dispatcher :
+    const dispatch = createEventDispatcher();
+
+    // Value updater :
+    function handleInput(event: Event) : void {
+        value = event.target["value"] ?? "";
+    }
+</script>
+
+<div class="flex flex-col-reverse">
+    <label for={type} class="text-sm text-white uppercase ml-1 font-bold">{title}</label>
+    <input 
+        type={type} placeholder={placeholder} 
+        class="rounded outline-none px-4 py-3 border-lg border-white focus:border-primary-1"
+        on:change={event => dispatch("change", { value, nativeEvent: event })}
+        on:input={handleInput}
+    >
+</div>
