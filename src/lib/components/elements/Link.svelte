351ba25d544065e92f1d2e268f0afd36da6b1d63
Bluzzi <camilledugas.pro@gmail.com>
2022-02-22 01:59:56 +0100

    Create Link component for text link

---
+++ src/lib/components/elements/Link.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    export let href: string | null = null;
+    export let target: "internal" | "external" = "internal";
+</script>
+
+<a {href} target={target === "external" ? "_blank" : ""} class="text-primary-1 hover:underline"><slot/></a>
