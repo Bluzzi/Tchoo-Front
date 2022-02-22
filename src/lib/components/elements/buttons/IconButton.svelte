d551e4929d614298ccce443ca4d39b0c6baef013
Bluzzi <camilledugas.pro@gmail.com>
2022-02-22 02:00:23 +0100

    Create IconButton component

---
+++ src/lib/components/elements/buttons/IconButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let src: string;
+    export let alt: string;
+</script>
+
+<div 
+    class="bg-gray-1 h-12 w-12 flex 
+    justify-center items-center 
+    rounded-full shadow
+    transform-all hover:scale-95
+    hover:brightness-105"
+>
+    <img {src} {alt} class="h-6 w-6">
+</div>
