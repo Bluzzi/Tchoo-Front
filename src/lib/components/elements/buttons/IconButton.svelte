7b56d433a3895be99ba89733203a035d0065de13
Bluzzi <camilledugas.pro@gmail.com>
2022-02-22 04:35:50 +0100

    IconButton (component): add onClick callback function

--- src/lib/components/elements/buttons/IconButton.svelte
+++ src/lib/components/elements/buttons/IconButton.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     export let src: string;
     export let alt: string;
+
+    export let onClick: () => void = () => {};
 </script>
 
 <div 
@@ -9,6 +11,7 @@
     rounded-full shadow
     transform-all hover:scale-95
     hover:brightness-105"
+    on:click={() => onClick()} 
 >
     <img {src} {alt} class="h-6 w-6">
 </div>
