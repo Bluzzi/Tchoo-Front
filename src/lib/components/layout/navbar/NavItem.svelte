fb60fe13de1d7e7b5edd6217efd1078be2635c42
Bluzzi <camilledugas.pro@gmail.com>
2022-02-23 00:40:56 +0100

    Move Navbar in Layout folder (and rewrite it)

---
+++ src/lib/components/layout/navbar/NavItem.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { navigating } from "$app/stores";
+    import { onMount } from "svelte";
+
+    export let route: string;
+
+    // Active state :
+    let active = false;
+
+    onMount(() => {
+        if(window) active = window.location.pathname === route;
+    });
+
+    navigating.subscribe(event => {
+        if(event) active = event.to.pathname === route;
+    });
+</script>
+
+<li 
+    class="text-white uppercase my-3 mx-4
+    border border-transparent hover:border-b-primary-1"
+    class:border-b-primary-1={active}
+><a href={route}><slot/></a></li>
---
+++ src/lib/components/layout/navbar/Navbar.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import Text from "$lib/components/elements/texts/Text.svelte";
+    import Button from "$lib/components/elements/buttons/Button.svelte";
+    import NavItem from "$lib/components/navigation/navbar/NavItem.svelte";
+    import IconButton from "$lib/components/elements/buttons/IconButton.svelte";
+    import { navigating } from "$app/stores";
+
+    let active = true;
+
+    navigating.subscribe(() => active = false);
+</script>
+
+<nav 
+    class:lg:hidden={!active}
+    class="bg-gray-2 h-navbar w-full
+    flex justify-around items-center
+    lg:h-screen lg:flex-col lg:justify-evenly lg:fixed"
+>
+    <Button background={false} href="/"><Text type="h1" size="large">THE TCHOOS</Text></Button>
+    
+    <ul class="flex lg:flex-col lg:items-center">
+        <NavItem route="/lottery">Lottery</NavItem>
+        <NavItem route="/leaderboard">Leaderboard</NavItem>
+        <NavItem route="/the-lab">The lab</NavItem>
+    </ul>
+
+    <div class="flex lg:flex-col lg:items-center">
+        <Button background={false}>Login</Button>
+        <Button>Sign in</Button>
+    </div>
+</nav>
+
+<span class="fixed right-5 bottom-5 hidden lg:block">
+    <IconButton 
+        src={active ? "/img/icon/navigation/xmark-white.svg" : "/img/icon/navigation/bars-white.svg"} 
+        alt="Open navbar" onClick={() => active = !active}
+    />
+</span>
