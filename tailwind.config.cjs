c1180a940b618897bd34d09498fa9f4f1b7727ba
Bluzzi <camilledugas.pro@gmail.com>
2022-02-23 00:47:41 +0100

    Tailwind Config: divers update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,17 +8,17 @@ module.exports = {
             "black": "black",
 
             "primary": {
-                1: "#F45776",
-                2: "#990A27"
+                1: "#F45776", // real primary
+                2: "#990A27" // for gradiant
             },
 
             "purple": {
-                1: "#4F0E81",
+                1: "#4F0E81", 
                 2: "#220836"
             },
             "gray": {
-                1: "#29272B",
-                2: "#231F25"
+                1: "#29272B", // for elements (button, cards...)
+                2: "#231F25" // for base background (navbar, section...)
             }
         },
 
@@ -32,6 +32,11 @@ module.exports = {
             "xl": "48px"
         },
 
+        backgroundImage: {
+            "sun-tornado": "url('/img/background/sun-tornado.svg')",
+            "sun-tornado-animated": "url('/img/background/sun-tornado-animated.svg')"
+        },
+
         borderWidth: {
             DEFAULT: "2px",
             lg: "3px"
@@ -43,7 +48,8 @@ module.exports = {
         },
 
         boxShadow: {
-            DEFAULT: "0 0 7px rgba(0 0 0 / 0.2)"
+            DEFAULT: "0 0 7px rgba(0 0 0 / 0.2)",
+            "white": "0 0 10px rgba(255 255 255 / 0.05)"
         },
 
         screens: {
@@ -55,6 +61,11 @@ module.exports = {
         },
 
         extend: {
+            height: {
+                "navbar": "4rem",
+                "navbar-adjust": "calc(100vh - 4rem)"
+            },
+
             scale: {
                 "98": "0.98"
             },
