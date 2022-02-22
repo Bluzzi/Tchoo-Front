345ed9c84eed2a27b1c2ca00ce130d00faa804a2
Bluzzi <camilledugas.pro@gmail.com>
2022-02-23 00:56:13 +0100

    Button (component): remove useless style

--- src/lib/components/elements/buttons/Button.svelte
+++ src/lib/components/elements/buttons/Button.svelte
@@ -15,7 +15,7 @@
     const buttonStyles = {
         base: `
             border border-primary-1
-            font-sans text-white uppercase 
+            text-white uppercase 
             px-4 py-2 m-2 rounded shadow-sm
             flex justify-center items-center
         `,
