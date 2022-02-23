0a75caa9bb01d7b5574fd79770d7a73741ab1ecd
Bluzzi <camilledugas.pro@gmail.com>
2022-02-23 13:06:35 +0100

    CardClose (box component): use Card component

--- src/lib/components/container/CardClose.svelte
+++ src/lib/components/container/CardClose.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
     import Text from "$lib/components/elements/texts/Text.svelte";
+    import Card from "$lib/components/container/Card.svelte";
 
     export let title: string;
 
     let card: HTMLDivElement;
 </script>
 
-<div bind:this={card} class="bg-gray-1 w-128 py-8 px-7 rounded">
-    <div class="flex items-center justify-between pb-5">
-        <Text size="large">{title.toUpperCase()}</Text>
+<div bind:this={card}>
+    <Card>
+        <div class="flex items-center justify-between pb-5">
+            <Text size="large">{title.toUpperCase()}</Text>
 
-        <span 
-            class="bg-gray-2 h-10 w-14 rounded flex items-center justify-center hover:bg-primary-1"
-            on:click={() => card.remove()}
-        >
-            <img 
-                src="/img/icon/navigation/xmark-white.svg" 
-                alt="close" class="h-6 w-6"
+            <span 
+                class="bg-gray-2 h-10 w-14 rounded flex items-center justify-center hover:bg-primary-1"
+                on:click={() => card.remove()}
             >
-        </span>
-    </div>
+                <img 
+                    src="/img/icon/navigation/xmark-white.svg" 
+                    alt="close" class="h-6 w-6"
+                >
+            </span>
+        </div>
 
-    <slot/>    
+        <slot/>  
+    </Card>
 </div>
