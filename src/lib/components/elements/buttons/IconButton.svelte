7908152e839d2055a81b6a7f33ac0ddacb331572
Bluzzi <camilledugas.pro@gmail.com>
2022-02-27 20:12:52 +0100

    IconButton (component): update
    - use click event instead callback
    - add href and target attributes
    - clean the code
    - update all file that depend on this component

--- src/lib/components/elements/buttons/IconButton.svelte
+++ src/lib/components/elements/buttons/IconButton.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
+    import { createEventDispatcher } from "svelte";
+
+    // Attributes :
     export let src: string;
     export let alt: string;
 
-    export let onClick: () => void = () => {};
+    export let href: string | null = "javascript:void(0);";
+    export let target: "_self" | "_blank" = "_self";
+
+    // Dispatcher :
+    const dispatch = createEventDispatcher();
 </script>
 
-<div 
+<a 
+    {href} target={target === "_blank" ? "_blank" : ""} 
+    on:click={e => dispatch("click", { nativeEvent: e })} 
     class="bg-gray-1 h-12 w-12 flex 
-    justify-center items-center 
-    rounded-full shadow
-    transform-all hover:scale-95
-    hover:brightness-105"
-    on:click={() => onClick()} 
->
-    <img {src} {alt} class="h-6 w-6">
-</div>
+        justify-center items-center 
+        rounded-full shadow
+        transform-all hover:scale-95
+        hover:brightness-105"
+><img {src} {alt} class="h-6 w-6"></a>
