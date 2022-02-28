1f94627c506d61247cd9dfb3b09400db6ac7adac
Bluzzi <camilledugas.pro@gmail.com>
2022-02-28 21:48:09 +0100

    Redirect (utils): add JSdoc for redirectByParam() function

--- src/lib/utils/Redirect.ts
+++ src/lib/utils/Redirect.ts
@@ -1,5 +1,8 @@
 import { goto } from "$app/navigation";
 
+/**
+ * Goto the path in the "redirect" parameter in pathname.
+ */
 export function redirectByParam(location: Location) : void {
     const param = new URLSearchParams(location.search).get("redirect");
 
