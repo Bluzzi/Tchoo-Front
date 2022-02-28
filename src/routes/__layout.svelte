1421b9bf16e3512dd100c5416c872dc598d4d4c8
Bluzzi <camilledugas.pro@gmail.com>
2022-02-28 21:49:47 +0100

    Layout: check if API token is always valid

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,11 @@
     import Navbar from "$lib/components/layout/navbar/Navbar.svelte";
     import MediaLinks from "$lib/components/layout/MediaLinks.svelte";
     import Notifications from "$lib/components/layout/notification/Notifications.svelte";
+    import { updateConnection } from "$lib/api/AccountStore";
     import "/static/css/tailwind.css"
+    import { onMount } from "svelte";
+
+    onMount(() => window && updateConnection(window.location));
 </script>
 
 <div class="min-h-screen max-w-full bg-purple-2">
