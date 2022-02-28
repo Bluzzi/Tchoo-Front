8ae98f0167b594c0e0e52df832604f9d2112d5f2
Bluzzi <camilledugas.pro@gmail.com>
2022-02-28 15:14:20 +0100

    Navbar (component): clean code and comment

--- src/lib/components/layout/navbar/Navbar.svelte
+++ src/lib/components/layout/navbar/Navbar.svelte
@@ -4,20 +4,36 @@
     import NavItem from "$lib/components/layout/navbar/NavItem.svelte";
     import IconButton from "$lib/components/elements/buttons/IconButton.svelte";
     import { navigating } from "$app/stores";
+    import { token, username, wallet } from "$lib/api/endpoints/Account";
+    import { goto } from "$app/navigation";
 
+    // Active state for mobile device :
     let active = true;
 
     navigating.subscribe(() => active = false);
+
+    // Redirect function for login/register pages :
+    function connect(type: "login" | "register") : void {
+        const currentPath = window.location.pathname;
+
+        const route = type === "login" ? "/connect" : "/connect/register";
+        const redirect = currentPath && !["/", "/connect", "/connect/register"].includes(currentPath) ? 
+            "?redirect=" + currentPath : "";
+
+        goto(route + redirect);
+    }
 </script>
 
 <nav 
     class:lg:hidden={!active}
     class="bg-gray-2 h-navbar w-full
     flex justify-around items-center
-    lg:h-screen lg:flex-col lg:justify-evenly lg:fixed"
+    lg:h-screen lg:flex-col lg:justify-evenly lg:fixed lg:z-50"
 >
-    <Button background={false} href="/"><Text type="h1" size="large">THE TCHOOS</Text></Button>
+    <!-- Title -->
+    <Button theme="void" href="/"><Text type="h1" size="large">THE TCHOOS</Text></Button>
     
+    <!-- Navbar elements -->
     <ul class="flex lg:flex-col lg:items-center">
         <NavItem route="/lottery">Lottery</NavItem>
         <NavItem route="/leaderboard">Leaderboard</NavItem>
@@ -25,12 +41,39 @@
     </ul>
 
     <div class="flex lg:flex-col lg:items-center">
-        <Button background={false}>Login</Button>
-        <Button>Sign in</Button>
+        {#if $token}
+            {#if $wallet}
+                <!-- Login and wallet linked -->
+                <Button theme="void" uppercase={false}>@{$username}</Button>
+                
+                <span class="w-2"></span>
+                
+                <Button 
+                    theme="outline" uppercase={false}
+                    icon="/img/icon/other/link-primary.svg" 
+                    href="/connect/wallet"
+                >{$wallet.slice(0, 3)}...{$wallet.slice($wallet.length - 4)}</Button>
+            {:else}
+                <!-- Login but wallet not linked -->
+                <Button theme="void" uppercase={false}>@{$username}</Button>
+
+                <span class="w-2"></span>
+
+                <Button href="/connect/wallet">Link wallet</Button>
+            {/if}
+        {:else}
+            <!-- Not login -->
+            <Button theme="void" on:click={() => connect("login")}>Login</Button>
+
+            <span class="w-2"></span>
+
+            <Button on:click={() => connect("register")}>Sign up</Button>
+        {/if}
     </div>
 </nav>
 
-<span class="fixed right-5 bottom-5 hidden lg:block">
+<!-- Mobile icon for open navbar -->
+<span class="fixed right-5 bottom-5 hidden lg:block lg:z-50">
     <IconButton 
         src={active ? "/img/icon/navigation/xmark-white.svg" : "/img/icon/navigation/bars-white.svg"} 
         alt="Open navbar" on:click={() => active = !active}
