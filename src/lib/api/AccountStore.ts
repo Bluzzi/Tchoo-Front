f8fadf8ab9743ad76c52ad77166dff258456e230
Bluzzi <camilledugas.pro@gmail.com>
2022-02-28 21:50:39 +0100

    AccountStore (store): create updateConnection() function

--- src/lib/api/AccountStore.ts
+++ src/lib/api/AccountStore.ts
@@ -1,5 +1,7 @@
-import { writable } from "svelte/store";
+import { get, writable } from "svelte/store";
 import { browser } from "$app/env";
+import { isTokenValid, updateInfo } from "./endpoints/Account";
+import { gotoConnection } from "$lib/utils/Redirect";
 
 interface Wallet {
     address: string;
@@ -8,8 +10,26 @@ interface Wallet {
 
 export const token = writable(browser && localStorage.getItem("token"));
 export const username = writable(browser && localStorage.getItem("username"));
-export const wallet = writable<Wallet | null>(browser && JSON.parse(localStorage.getItem("wallet")));
+export const wallet = writable<Wallet | null>(
+    browser && localStorage.getItem("wallet") ? JSON.parse(localStorage.getItem("wallet")) : null
+);
 
-token.subscribe(value => browser && value && localStorage.setItem("token", value));
-username.subscribe(value => browser && value && localStorage.setItem("username", value));
-wallet.subscribe(value => browser && value && localStorage.setItem("wallet", JSON.stringify(value)));
+token.subscribe(value => browser && localStorage.setItem("token", value ?? ""));
+username.subscribe(value => browser && localStorage.setItem("username", value ?? ""));
+wallet.subscribe(value => browser && localStorage.setItem("wallet", value ? JSON.stringify(value) : ""));
+
+export async function updateConnection(location: Location) : Promise<void> {
+    if(!get(token)) return;
+
+    const tokenIsValid = (await isTokenValid()).success;
+
+    if(tokenIsValid){
+        updateInfo();
+    } else {
+        token.set("");
+        username.set("");
+        wallet.set(null);
+
+        gotoConnection("login", location);
+    }
+}
