69d779ff9f3c905e9b40448d9352f0f6b89885e9
Bluzzi <camilledugas.pro@gmail.com>
2022-03-05 03:16:59 +0100

    Pets (api endpoint): create getLeaderboard() request

---
+++ src/lib/api/endpoints/Pets.ts
@@ -0,0 +1,31 @@
+import { jsonFetch } from "$lib/utils/Fetch";
+import { apiLink } from "../Information";
+import { request, ResponseStatus } from "../RequestManager";
+
+export interface Leaderboard {
+    total_count: number;
+    top_nfts: NFT[];
+}
+
+export interface NFT {
+    nonce: number;
+
+    name: string;
+    holder_username: string;
+
+    two_d_picture: string;
+
+    prestige_balance: number;
+}
+
+export async function getLeaderboard(min: number, max: number) : Promise<ResponseStatus<Leaderboard>> {
+    const response = await jsonFetch<Leaderboard>(apiLink + "/pets/get_top", {
+        method: "POST",
+        body: JSON.stringify({
+            start_index: min,
+            stop_index: max
+        })
+    });
+
+    return await request(response, "Pets list received");
+}
