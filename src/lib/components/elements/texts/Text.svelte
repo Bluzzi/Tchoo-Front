4c40ce7e7ca44dac9937510be9ce819186be3f77
Bluzzi <camilledugas.pro@gmail.com>
2022-03-05 03:48:21 +0100

    Text (component): add header attribute

--- src/lib/components/elements/texts/Text.svelte
+++ src/lib/components/elements/texts/Text.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     export let type: "p" | "h2" | "h1" = "p";
     export let size: "small" | "base" | "large" | "xl" = "base"; 
+    
+    export let header = false;
 
     const styles = {
         small: "text-sm",
@@ -11,9 +13,9 @@
 </script>
 
 {#if type === "p"}
-    <p class={"text-white " + styles[size]}><slot/></p>
+    <p class={"text-white " + styles[size]} class:my-14={header}><slot/></p>
 {:else if type === "h2"}
-    <h2 class={"text-white " + styles[size]}><slot/></h2>
+    <h2 class={"text-white " + styles[size]} class:my-14={header}><slot/></h2>
 {:else}
-    <h1 class={"text-white " +  styles[size]}><slot/></h1>
+    <h1 class={"text-white " +  styles[size]} class:my-14={header}><slot/></h1>
 {/if}
