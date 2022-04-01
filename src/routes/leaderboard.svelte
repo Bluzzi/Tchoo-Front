ff675657b489d492b57d8b7289979720570a1d97
Bluzzi <camilledugas.pro@gmail.com>
2022-04-01 21:12:51 +0200

    Create Leaderboard page

--- src/routes/leaderboard.svelte
+++ src/routes/leaderboard.svelte
@@ -1,8 +1,13 @@
-<script>
+<script lang="ts">
     import Text from "$lib/components/elements/texts/Text.svelte";
     import Title from "$lib/components/elements/Title.svelte";
+    import Leaderboard from "$lib/components/layout/Leaderboard.svelte";
 </script>
 
 <Title text="Leaderboard"/>
 
-<Text>Leaderboard</Text>
+<div class="flex flex-col items-center">
+    <Text size="xl" header>LEADERBOARD</Text>
+
+    <Leaderboard/>
+</div>
