73ba0817d552297e9a61248642230973d8ec9793
Bluzzi <camilledugas.pro@gmail.com>
2022-04-07 21:35:08 +0200

    Create error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+    import type { ErrorLoadInput } from "@sveltejs/kit";
+
+    export function load({ error, status }: ErrorLoadInput) {
+        return {
+            props: {
+                title: `${status}: ${error.message}`
+            }
+        };
+    }
+</script>
+
+<script>
+    export let title;
+</script>
+
+<h1>{title}</h1>
