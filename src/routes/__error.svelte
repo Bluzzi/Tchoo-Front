dd647dddb04ef706a3a550f8689a0f314c47d29f
Bluzzi <camilledugas.pro@gmail.com>
2022-04-08 19:32:17 +0200

    Error (page): finish the page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,17 +1,32 @@
 <script context="module" lang="ts">
-    import type { ErrorLoadInput } from "@sveltejs/kit";
+    import type { ErrorLoadInput, LoadOutput } from "@sveltejs/kit";
 
-    export function load({ error, status }: ErrorLoadInput) {
+    export function load({ error, status }: ErrorLoadInput) : LoadOutput {
         return {
             props: {
-                title: `${status}: ${error.message}`
+                statusCode: status,
+                message: error.message
             }
         };
     }
 </script>
 
-<script>
-    export let title;
+<script lang="ts">
+    import Button from "$lib/components/elements/buttons/Button.svelte";
+    import Text from "$lib/components/elements/texts/Text.svelte";
+    import Title from "$lib/components/elements/Title.svelte";
+
+    export let statusCode: string;
+    export let message: string;
 </script>
 
-<h1>{title}</h1>
+<Title text={"Error " + statusCode}/>
+
+<div class="grid place-content-center h-navbar-adjust">
+    <Text size="xl">Error {statusCode}</Text>
+    <Text size="large">{message}</Text>
+
+    <span class="h-5"></span>
+
+    <Button href="/">Back to home</Button>
+</div>
