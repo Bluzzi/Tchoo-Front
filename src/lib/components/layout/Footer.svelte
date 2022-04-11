d08f25df84213916d93147d43d51d335c6aac0c1
Bluzzi <camilledugas.pro@gmail.com>
2022-04-11 18:21:51 +0200

    Create the Footer component

---
+++ src/lib/components/layout/Footer.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Text from "../elements/texts/Text.svelte";
+    import Link from "../elements/texts/Link.svelte";
+</script>
+
+<div class="w-full h-16 shadow bg-gray-2 mt-20 grid place-content-center">
+    <Text>
+        Made with ❤️ by the Meta Friend team • 
+        <Link>Privacy Policy</Link> • 
+        <Link>Terms and Conditions of Sale</Link></Text>
+</div>
