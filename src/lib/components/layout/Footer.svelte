226dbe9d9bcd717da53e0c441de9a7825e8bff72
Bluzzi <camilledugas.pro@gmail.com>
2022-04-14 11:31:47 +0200

    Responsive fixs

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -3,9 +3,10 @@
     import Link from "../elements/texts/Link.svelte";
 </script>
 
-<div class="w-full h-16 shadow bg-gray-2 mt-20 grid place-content-center">
+<div class="grid place-content-center h-max w-full py-10 px-8 lg:pb-20 shadow bg-gray-2 mt-20 text-center">
     <Text>
         Made with ❤️ by the Meta Friend team • 
         <Link>Privacy Policy</Link> • 
-        <Link>Terms and Conditions of Sale</Link></Text>
+        <Link>Terms and Conditions of Sale</Link>
+    </Text>
 </div>
