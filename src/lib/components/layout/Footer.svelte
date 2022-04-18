724ba45be62e8b12993272510f863c497bfde591
Bluzzi <camilledugas.pro@gmail.com>
2022-04-19 01:53:30 +0200

    Footer (component): add links to the legal documents pages

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -6,7 +6,7 @@
 <div class="grid place-content-center h-max w-full py-10 px-8 lg:pb-20 shadow bg-gray-2 mt-20 text-center">
     <Text>
         Made with ❤️ by the Meta Friend team • 
-        <Link>Privacy Policy</Link> • 
-        <Link>Terms and Conditions of Sale</Link>
+        <Link href="/legal/privacy-policy">Privacy Policy</Link> • 
+        <Link href="/legal/conditions-of-sale">Terms and Conditions of Sale</Link>
     </Text>
 </div>
