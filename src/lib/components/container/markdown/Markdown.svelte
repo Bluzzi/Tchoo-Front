b6f7fedf4d02662841381a57ef8fb92f3e53f841
Bluzzi <camilledugas.pro@gmail.com>
2022-04-19 18:53:30 +0200

    Create Markdown component for parse markdown file into HTML

---
+++ src/lib/components/container/markdown/Markdown.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { fromMarkdown } from "mdast-util-from-markdown";
+    import MarkdownElement from "./MarkdownElement.svelte";
+
+    export let source: string;
+
+    const jsonMarkdown = fromMarkdown(source).children;
+</script>
+
+{#each jsonMarkdown as element}
+    <MarkdownElement element={element}/>
+{/each}
