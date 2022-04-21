6dad6178b033597ed27f7205dfbbeaccd927cab0
Bluzzi <camilledugas.pro@gmail.com>
2022-04-21 19:45:20 +0200

    Card (component): add clickable possibility

--- src/lib/components/container/Card.svelte
+++ src/lib/components/container/Card.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+    import { createEventDispatcher } from "svelte";
+
     export let pySize: "small" | "base" = "base";
+
+    export let clickable: boolean = false;
+    export let clicked: boolean = false;
+
+    // Event dispatcher :
+    const dispatch = createEventDispatcher();
 </script>
 
 <div 
-    class="bg-gray-1 w-full px-6 rounded shadow" 
+    class="bg-gray-1 w-full px-6 rounded shadow
+        border border-transparent" 
+    class:hover:border-primary-1={clickable}
+    class:!border-primary-1={clicked}
     class:py-8={pySize === "base"} 
     class:py-4={pySize === "small"}
+    on:click={event => dispatch("click", { nativeEvent: event })}
 ><slot/></div>
