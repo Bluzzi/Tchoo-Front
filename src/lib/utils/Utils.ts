97b5b158aeccc0a1fbf4f7352c756b4980d6e0a6
Bluzzi <camilledugas.pro@gmail.com>
2022-04-21 19:46:32 +0200

    Utils (utils): create firstLetterToUpperCase() function

--- src/lib/utils/Utils.ts
+++ src/lib/utils/Utils.ts
@@ -1,3 +1,7 @@
 export function range(start: number, end: number) : number[] {
     return Array.from(Array(end - start + 1), (_, i) => i + start);
 }
+
+export function firstLetterToUpperCase(message: string) : string {
+    return message.charAt(0).toUpperCase() + message.slice(1);
+}
